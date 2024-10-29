4893244a7be70f3e9067e87b2f90414511a374bd
mohamad <mohamadshiravi85@gmail.com>
2024-10-29 16:13:51 +0330

    update security

---
+++ app/loading.js
@@ -0,0 +1,9 @@
+import { SiTodoist } from "react-icons/si";
+
+export default function RootLoading() {
+  return (
+    <section className="flex items-center justify-center w-full h-screen text-orange-600 text-9xl">
+      <SiTodoist />
+    </section>
+  );
+}
